50d47261454ab3f5da62a602ee7338561e253cd2
water1995 <genevievexal@hotmail.com>
2020-11-22 18:52:53 +0530

    bklist 2

---
+++ book list/app.js
@@ -0,0 +1,47 @@
+function Book(title,author,isbn){
+
+  this.title = title;
+  this.author = author;
+  this.isbn = isbn;
+}
+
+function UI(){}
+
+UI.prototype.addBookToList = function(book){
+
+  const list = document.getElementById('book-list');
+
+  const row = document.createElement('tr');
+
+  row.innerHTML = `<td>${book.title}</td>
+                   <td>${book.author}</td>
+                   <td>${book.isbn}</td>
+                   <td><a href="#" class="delete">X</a></td>
+                   `;
+
+  list.appendChild(row);
+}
+
+UI.prototype.clearField = function(){
+
+  document.getElementById('title').value = '';
+  document.getElementById('author').value = '';
+  document.getElementById('isbn').value = '';
+}
+
+document.getElementById('book-form').addEventListener('submit',function(e){
+
+  const title = document.getElementById('title').value,
+        author = document.getElementById('author').value,
+        isbn = document.getElementById('isbn').value;
+
+  const book = new Book(title,author,isbn);
+
+  const ui = new UI();
+
+  ui.addBookToList(book);
+  ui.clearField();
+
+  e.preventDefault();
+
+})

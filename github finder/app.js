4a6e6e2dffcc6d21f0b4e9ca1e32c3e35b9a0d37
water1995 <genevievexal@hotmail.com>
2021-02-01 21:05:34 +0530

    1/2/2021

--- github finder/app.js
+++ github finder/app.js
@@ -22,6 +22,7 @@ searchUser.addEventListener('keyup',(e) => {
 
         //Show Profile
         ui.showProfile(data.profile);
+        ui.showRepos(data.repos);
 
       }
     })
--- github finder/github.js
+++ github finder/github.js
@@ -2,19 +2,24 @@ class Github{
 
   constructor(){
 
-    this.client_id ='30745bbcc72a7054cdf0';
-    this.client_secret = '6a0c365fc61fa4893de6b1018e24e8441befca97';
+    
+    this.repos_count = 5;
+    this.repos_sort = 'created:asc';
   }
 
   async getUser(user){
 
     const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+    const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+    
+    
     const profile = await profileResponse.json();
+    const repos = await repoResponse.json();
 
     console.log("Profile "+profile);
 
     return{
-      profile
+      profile,repos
     }
   }
 }
